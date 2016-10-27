561a399354797fea7a7d83369e8eecb175cb03d1
Alex Pamphilon <apamphilon@yahoo.com>
2016-10-27 19:39:56 +0100

    add key functionality - need to fix equals/return

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 // delare variables
 var screen = $('#screen'),
     numberButton = $('.number'),
-	  buttonValue = '',
+	  inputValue = '',
 	  query = [],
 	  queryStr = '',
 	  equalsButton = $('#equals'),
-	  ceButton = $('#ce');
+	  ceButton = $('#ce'),
     screenVal = '';
 
 // functions
 // construct query
 function constructQuery() {
-	query.push(buttonValue);
 	queryStr = query.join('');
-
 	setScreenValue();
 }
 
@@ -31,28 +29,64 @@ function calcTotal(a) {
 	return evaluate;
 }
 
-// program
-// get button value on click
-numberButton.on('click', function() {
-	buttonValue = $(this).data('val');
-
-   // construct query
-   constructQuery();
-});
-
-// equals button on click
-equalsButton.on('click', function() {
-	var result = calcTotal(queryStr);
-	screen.text(result);
+// display total
+function displayTotal() {
+  var result = calcTotal(queryStr);
+  screen.text(result);
 
   if ( screen.text() === "37047734" ) {
     alert('hellhole');
     $('#calculator').addClass('rotated');
   }
+}
+
+// reset query
+function resetQuery() {
+  screen.text('');
+  query = [];
+}
+
+
+// program
+// get button value on click
+numberButton.on('click', function() {
+	inputValue = $(this).data('val');
+	query.push(inputValue);
+
+  // construct query
+  constructQuery();
+});
+
+// get key pressed
+document.onkeypress = function(evt) {
+  evt = evt || window.event;
+  var charCode = evt.keyCode || evt.which;
+  var inputValue = String.fromCharCode(charCode);
+
+
+  // create an array of allowed keys
+  var allowedKeys = ['0','1','2','3','4','5','6','7','8','9','+','-','*'];
+
+  // push to array if in allowedKeys
+  if ( allowedKeys.indexOf(inputValue) > -1 ) {
+    query.push(inputValue);
+  }
+
+  // if is return or =
+  if ( charCode == '13' || charCode == '61' ) {
+    displayTotal();
+  }
+
+  // construct query
+  constructQuery();
+};
+
+// equals button on click
+equalsButton.on('click', function() {
+  displayTotal();
 });
 
 // ce button on click
 ceButton.on('click', function() {
-	screen.text('');
-  query = [];
+  resetQuery();
 });
